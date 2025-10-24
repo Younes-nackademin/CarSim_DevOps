4da73227b19bee41204402280bd3cadf515cedd1
younes-nackademin <younes.kesraoui@yh.nackademin.se>
2025-10-24 02:55:14 +0200

    Add project files.

---
+++ CarSimulator.Tests/DriveForwardTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using DataLogicLibrary.DirectionStrategies;
+using DataLogicLibrary.DTO;
+using DataLogicLibrary.Infrastructure.Enums;
+
+namespace CarSimulator.Tests
+{
+    public class DriveForwardTests
+    {
+        [Fact]
+        public void Execute_WhenMovingForward_ShouldKeepSameDirection()
+        {
+            var strategy = new DriveForwardStrategy();
+            var status = new StatusDTO
+            {
+                CardinalDirection = CardinalDirection.North,
+                MovementAction = MovementAction.Forward
+            };
+
+            var result = strategy.Execute(status);
+
+            Assert.Equal(CardinalDirection.North, result.CardinalDirection);
+        }
+
+        [Fact]
+        public void Execute_WhenMovingBackward_ShouldReverseDirection()
+        {
+            var strategy = new DriveForwardStrategy();
+            var status = new StatusDTO
+            {
+                CardinalDirection = CardinalDirection.East,
+                MovementAction = MovementAction.Backward
+            };
+
+            var result = strategy.Execute(status);
+
+            Assert.Equal(CardinalDirection.West, result.CardinalDirection);
+        }
+    }
+}
---
+++ CarSimulator.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CarSimulator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
